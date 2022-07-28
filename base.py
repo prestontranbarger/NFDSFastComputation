80cd8e579509b678e824bb0d80edc87b52a11323
prestontranbarger <preston.tranbarger@gmail.com>
2022-07-27 21:01:11 -0500

    slight revisions

--- base.py
+++ base.py
@@ -2,31 +2,23 @@ from sage.all import *
 from tqdm import tqdm
 
 def matrixString(m):
-  return str(m[0][0]) + "," + str(m[0][1]) + "," + str(m[1][0]) + "," + str(m[1][1]) + ":"
+    return str(m[0][0]) + "," + str(m[0][1]) + "," + str(m[1][0]) + "," + str(m[1][1]) + ":"
 
 def stringMatrix(s):
-  l = s[:-1].split(",")
-  return matrix(ZZ, [[int(l[0]), int(l[1])],
-                     [int(l[2]), int(l[3])]])
+    l = s[:-1].split(",")
+    return matrix(ZZ, [[int(l[0]), int(l[1])],
+                       [int(l[2]), int(l[3])]])
 
 def complexString(z):
-  z += 0 * I
-  return "(" + str(float(z.real())) + "," + str(float(z.imag())) + ")"
+    z += 0 * I
+    return "(" + str(float(z.real())) + "," + str(float(z.imag())) + ")"
 
 def stringComplex(s):
-  l = s[1:-1].split(",")
-  return float(l[0]) + float(l[1]) * I
+    l = s[1:-1].split(",")
+    return float(l[0]) + float(l[1]) * I
 
-def dCharString(dChar):
-  l = str(dChar).split()
-  out = l[3] + "c" + l[6] + ";"
-  Tn = totient(modulus(dChar))
-  zTn = CyclotomicField(Tn).gen()
-  ns = [ss.split(" |--> ")[0].split()[-1] for ss in str(dChar).split(",")]
-  for n in ns:
-    p = 0
-    for j in range(Tn):
-      if zTn ** j == dChar(int(n)):
-        p = j
-    out += n + "-" + str(p) + "-"
-  return out[:-1]
+def totient(n):
+    p = 1
+    for pe in f:
+        p *= (pe[0] - 1) * (pe[0] ** (pe[1] - 1))
+    return p
--- dirichletCharacters.py
+++ dirichletCharacters.py
@@ -11,3 +11,17 @@ def isPrimitive(dChar):
 
 def isEven(dChar):
     return dChar.is_even()
+
+def dCharString(dChar):
+    l = str(dChar).split()
+    out = l[3] + "c" + l[6] + ";"
+    Tn = totient(modulus(dChar))
+    zTn = CyclotomicField(Tn).gen()
+    ns = [ss.split(" |--> ")[0].split()[-1] for ss in str(dChar).split(",")]
+    for n in ns:
+        p = 0
+        for j in range(Tn):
+            if zTn ** j == dChar(int(n)):
+                p = j
+        out += n + "-" + str(p) + "-"
+    return out[:-1]
